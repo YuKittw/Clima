677d9986564ec2d11d9d9d1782e18b823a5068db
YuKittw <matheusaristides36@gmail.com>
2024-06-01 12:13:14 -0300

    clima

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 
+window.addEventListener('load', () => {
 
-const getwheaher = () =>{
-fetch('http://api.weatherapi.com/v1/current.json?key=aa9cad4dddca40b8af5121111240106&q=Joao-Pessoa&lang=pt')
-.then((Response)=>{  
-    Response.json()
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        const getwheaher = async () => {
+            const getData = await fetch('http://api.weatherapi.com/v1/current.json?key=aa9cad4dddca40b8af5121111240106&q=Joao-Pessoa&lang=pt')
+            const res = await getData.json()
+
+            Response.json()
+    console.log(res.current.codition.text)
+    console.log(res.location.name)
+    console.log(res.current.temp_c)
+    console.log(res.current.feelslike_c)
+    console.log(res.current.wind_kph)
+
+const codicao = document.createElement('p')
+codicao.innerHTML = res_current.Response
+codition.text
+document.querySelectorery("dados")
+appendchild(codicao)
+        }
+    })
+    getwheaher()
 })
+   
 
-.then((Response)=>console.log(Response))
-}
-
-getwheaher()
